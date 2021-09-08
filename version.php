863e94d9035cd00e7681775b25b69c146ab37d9b
Stefan Hanauska <stefan.hanauska@altmuehlnet.de>
2021-09-08 20:23:09 +0200

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_user';
-$plugin->release = '0.1.9';
-$plugin->version = 2021090503;
+$plugin->release = '0.1.10';
+$plugin->version = 2021090801;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
